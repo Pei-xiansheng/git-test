27bedfad26caf7c2765a41fa582087a120787ebb
Evan <Evan@gmail.com>
2021-09-30 07:51:57 +0800

    Second commit!

--- src/main/java/com/zap/test/GitTest.java
+++ src/main/java/com/zap/test/GitTest.java
@@ -11,5 +11,6 @@ package com.zap.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Word!");
+        System.out.println("Hello！");
     }
 }
