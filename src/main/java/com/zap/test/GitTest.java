856d8b3107ddc60d176e8e60fb0ef1c86d8c0984
Evan <Evan@gmail.com>
2021-09-30 07:50:43 +0800

    First Commit！

---
+++ src/main/java/com/zap/test/GitTest.java
@@ -0,0 +1,15 @@
+package com.zap.test;
+
+/**
+ * @author: ZhuApeng
+ * @Title: GitTest
+ * @ProjectName: RedisPro
+ * @Description:
+ * @date: 2021/9/30 7:48
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
